24136ca3e14e2fc8d1aa295ce9ba16fb9b45b73c
Xuân Đại <daik5xuanvinh@gmail.com>
2026-03-24 15:46:07 +0700

    Add files via upload

---
+++ console_14.sql
@@ -0,0 +1,51 @@
+create table products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    stock INT
+);
+
+select * from products;
+
+insert into products(name, category, price, stock)
+values
+    ('Laptop Dell', 'Electronics', 1500.00, 5),
+    ('Chuột Logitech', 'Electronics', 25.50, 50),
+    ('Bàn phím Razer', 'Electronics', 120.00, 20),
+    ('Tủ lạnh LG', 'Home Appliances', 800.00, 3),
+    ('Máy giặt Samsung', 'Home Appliances', 600.00, 2);
+
+insert into products(name, category, price, stock)
+values ('Điều hòa Panasonic', 'Home Appliances', 400.00, 10);
+
+update products
+set stock = 7
+where name = 'Laptop Dell';
+
+delete from products
+where stock = 0;
+
+select *
+from products
+order by price;
+
+select distinct category
+from products;
+
+select *
+from products
+where price between 100 and 1000;
+
+select *
+from products
+where name ilike '%lg%'
+   or name ilike '%samsung%';
+
+select *
+from products
+order by price desc
+--limit 2;
+limit 2 offset 1;
+
+delete from students;
